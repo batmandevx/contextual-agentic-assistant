1db33be847eeef094b5a64305049bf560695b1d5
Ayush Upadhyay <ayushanimationgamer@gmail.com>
2026-01-16 05:32:27 +0530

    Add S3 bucket policy for CloudFront OAI access

--- terraform/main.tf
+++ terraform/main.tf
@@ -480,6 +480,28 @@ resource "aws_s3_bucket_public_access_block" "frontend" {
   restrict_public_buckets = true
 }
 
+# S3 bucket policy to allow CloudFront OAI to read objects
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontOAI"
+        Effect    = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.frontend.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.frontend.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.frontend]
+}
+
 # CloudFront Distribution
 resource "aws_cloudfront_origin_access_identity" "frontend" {
   comment = "OAI for ${var.project_name} frontend"
